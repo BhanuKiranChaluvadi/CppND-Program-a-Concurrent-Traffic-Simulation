f102190b2832db462085d81744c1829fdb25b902
BhanuKiranChaluvadi <nthdimensionalreality@gmail.com>
2019-09-11 22:49:02 +0200

    adding logic to TrafficLight class

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -9,6 +9,10 @@
 // forward declarations to avoid include cycle
 class Vehicle;
 
+enum class TrafficLightPhase {
+    red ,
+    green
+};
 
 // FP.3 Define a class „MessageQueue“ which has the public methods send and receive. 
 // Send should take an rvalue reference of type TrafficLightPhase whereas receive should return this type. 
@@ -19,9 +23,13 @@ template <class T>
 class MessageQueue
 {
 public:
+    T receive();
+    void send(T &&msg);
 
 private:
-    
+    std::deque<TrafficLightPhase> _queue;
+    std::condition_variable _cond;
+    std::mutex _mutex;
 };
 
 // FP.1 : Define a class „TrafficLight“ which is a child class of TrafficObject. 
@@ -39,10 +47,7 @@ public:
     void waitForGreen();
     void simulate();
 
-    enum class TrafficLightPhase {
-        red ,
-        greeen
-    };
+
 
     // getters / setters
     TrafficLightPhase  getCurrentPhase();
@@ -55,7 +60,7 @@ private:
     // FP.4b : create a private member of type MessageQueue for messages of type TrafficLightPhase 
     // and use it within the infinite loop to push each new TrafficLightPhase into it by calling 
     // send in conjunction with move semantics.
-
+    MessageQueue<TrafficLightPhase> _messagequeue;
     TrafficLightPhase _currentPhase;
     std::condition_variable _condition;
     std::mutex _mutex;
